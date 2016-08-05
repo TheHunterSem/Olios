fb902be6c92b4d83bac59f220e8cab05e3310b9a
Vasyl <kiriljuk.vasja@gmail.com>
2016-08-05 14:57:47 +0300

    main screen

--- scripts/base.js
+++ scripts/base.js
@@ -2,3 +2,13 @@ $('.main-screen').slick({
 	dots: true,
 	arrows: false
 });
+
+$(document).ready(function() { 
+	$('a#right-menu-hamburger').click( function(event){ 
+		event.preventDefault();
+		 	$('#icon-menu').toggle();
+		 	$('#icon-cancel').toggle();
+		 		$('#main-right-menu').toggle();
+		 			
+		});
+	});
